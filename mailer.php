06ef68891aa7e382c0858f670e2a208bcc33f578
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-09-29 17:35:51 +0600

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -7,14 +7,14 @@ $mail->Host = "smtp.gmail.com";
 
 $mail->SMTPAuth = true;
 
-$mail->Username = "tawsiftorabi.jr@gmail.com";
-$mail->Password = "iloveorthimorethanfacebook";
+$mail->Username = ""; //Type in Email
+$mail->Password = "";//Type in password
 $mail->SMTPSecure = "ssl";
 $mail->Port = 465;
 $mail->Subject = "Test Subject";
 $mail->Body = "This is Body";
-$mail->setFrom("address:'tawsiftorabi.jr@gmail.com'", "name:'Tawsif Torabi'");
-$mail->addAddress("address:'editor@rupok.net'");
+$mail->setFrom("address:'".$mail->Username."'", "name:'Foo Bar'");
+$mail->addAddress("address:'random@random'");
 if($mail->send()){
 	echo "Sent";
 } else {
