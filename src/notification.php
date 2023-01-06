699068dea02e66c79cac6767412364c96b177a51
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2023-01-06 17:49:00 +0600

    Takes Application update from Server

---
+++ src/notification.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+//chk login status
+if(!isset($_SESSION['loggedin'])){
+	echo "<script>window.open('login.php','_self')</script>";
+	} else {
+		require('connect_db.php');
+	if(isset($_GET['data'])){
+		if($_GET['data']=='shorttext'){
+			$home = file_get_contents('http://grplusbd.net/app/magazine/notification.php?id=1&data=short');
+			echo $home;
+		}
+		if($_GET['data']=='more'){
+			?>
+			<html>
+				<head>
+					<link rel="stylesheet" type="text/css" href="style.css">		
+					<link href='login.css' rel='stylesheet'/>
+					<link href='orthi-lightbox/orthi-lightbox.css' rel='stylesheet'/>
+					<script src='orthi-lightbox/orthi-lightbox.js'></script>
+				</head>
+				<body style="background: white;">
+					<div id="nav">
+					<span class="nav-left">
+						<img src="img/	
+		<?php $logo = $db->query('SELECT * FROM settings where name="logo"')->fetchArray(SQLITE3_ASSOC)['value'];?>
+		<?php echo $logo; ?>" height="30px" style="vertical-align: text-top;"/>Application Update
+					</span>	
+					</div>
+					<div style="height: 60px;"></div>
+					<?php
+					$home = file_get_contents('http://grplusbd.net/app/magazine/notification.php?id=1&data=more');
+					echo $home;
+					?>
+				</body>
+			</html>
+				
+			<?php
+		}
+	}
+
+
+}
